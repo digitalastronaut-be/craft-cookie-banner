1049f0342dbf11e7e8232808da52d88016d41034
Bram Mortier <bram@digitalastronaut.be>
2025-12-04 14:27:09 +0100

    feat: removed alpine dependency

--- src/web/assets/src/js/banner.js
+++ src/web/assets/src/js/banner.js
@@ -1,93 +1,183 @@
 import { buildGoogleConsentV2Object } from "./utils.js";
 import { getCookie } from "./utils.js";
 
-export function banner() {
-	return {
-		detailedMode: false,
-		loading: false,
-		visible: false,
-		consent: {
+class CookieBanner extends HTMLElement {
+	constructor() {
+		super();
+
+		this.detailedSettingsElement = null;
+		this.categoriesListElement = null;
+
+		this.refuseAllButton = null;
+		this.determinePreferencesButton = null;
+		this.determinePreferencesLink = null;
+		this.acceptSelectedButton = null;
+		this.acceptAllButton = null;
+
+		this.manageConsentButtonElements = [];
+		this.consentCheckboxElements = [];
+
+		this.consent = {
 			essentialCookies: true,
 			functionalCookies: false,
 			analyticalCookies: false,
 			advertisementCookies: false,
 			personalizationCookies: false,
-		},
+		};
+	}
 
-		init() {
-			const existingCookieConsent = JSON.parse(getCookie("cookie_consent"));
+	connectedCallback() {
+		this.registerElements();
+		this.registerEventListeners();
 
-			if (!existingCookieConsent) this.visible = true;
+		const existingCookieConsent = JSON.parse(getCookie("cookie_consent"));
 
-			if (existingCookieConsent) {
-				this.consent = existingCookieConsent;
+		if (existingCookieConsent) this.consent = existingCookieConsent;
+		if (!existingCookieConsent) this.showBanner();
 
-				const googleConsentV2Object = buildGoogleConsentV2Object(this.consent);
-				gtag("consent", "update", googleConsentV2Object);
-			}
-		},
+		this.initializeConsentCheckboxes();
+		this.initializeBanner();
 
-		acceptAll() {
-			this.visible = false;
-			this.consent = {
-				essentialCookies: true,
-				functionalCookies: true,
-				analyticalCookies: true,
-				advertisementCookies: true,
-				personalizationCookies: true,
-			};
-
-			document.cookie = `cookie_consent=${JSON.stringify(this.consent)}; path=/; max-age=31536000; SameSite=Lax`;
-
+		if (existingCookieConsent) {
 			const googleConsentV2Object = buildGoogleConsentV2Object(this.consent);
 			gtag("consent", "update", googleConsentV2Object);
+		}
+	}
 
-			this.logConsent("Accept all");
-		},
+	acceptAll() {
+		this.hideBanner();
 
-		acceptSelected() {
-			this.visible = false;
-			document.cookie = `cookie_consent=${JSON.stringify(this.consent)}; path=/; max-age=31536000; SameSite=Lax`;
+		this.consent = {
+			essentialCookies: true,
+			functionalCookies: true,
+			analyticalCookies: true,
+			advertisementCookies: true,
+			personalizationCookies: true,
+		};
 
-			const googleConsentV2Object = buildGoogleConsentV2Object(this.consent);
-			gtag("consent", "update", googleConsentV2Object);
+		document.cookie = `cookie_consent=${JSON.stringify(this.consent)}; path=/; max-age=31536000; SameSite=Lax`;
 
-			this.logConsent("Accept selected");
-		},
+		const googleConsentV2Object = buildGoogleConsentV2Object(this.consent);
+		gtag("consent", "update", googleConsentV2Object);
 
-		refuseAll() {
-			this.visible = false;
-			this.consent = {
-				essentialCookies: true,
-				functionalCookies: false,
-				analyticalCookies: false,
-				advertisementCookies: false,
-				personalizationCookies: false,
-			};
+		this.logConsent("Accept all");
+	}
 
-			document.cookie = `cookie_consent=${JSON.stringify(this.consent)}; path=/; max-age=31536000; SameSite=Lax`;
+	acceptSelected() {
+		this.hideBanner();
 
-			const googleConsentV2Object = buildGoogleConsentV2Object(this.consent);
-			gtag("consent", "update", googleConsentV2Object);
+		document.cookie = `cookie_consent=${JSON.stringify(this.consent)}; path=/; max-age=31536000; SameSite=Lax`;
 
-			this.logConsent("Refuse all");
-		},
+		const googleConsentV2Object = buildGoogleConsentV2Object(this.consent);
+		gtag("consent", "update", googleConsentV2Object);
 
-		async logConsent(consentAction) {
-			const response = await fetch(`/cookie-banner/consent-records/create`, {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json",
-					"X-CSRF-Token": window.craftCookieBanner.csrfTokenValue,
-				},
-				body: JSON.stringify({
-					language: navigator.language,
-					consentCategories: this.consent,
-					consentAction,
-				}),
+		this.logConsent("Accept selected");
+	}
+
+	async logConsent(consentAction) {
+		const response = await fetch(`/cookie-banner/consent-records/create`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				"X-CSRF-Token": window.craftCookieBanner.csrfTokenValue,
+			},
+			body: JSON.stringify({
+				language: navigator.language,
+				consentCategories: this.consent,
+				consentAction,
+			}),
+		});
+
+		const data = await response.json();
+	}
+
+	showBanner() {
+		this.classList.add("visible");
+	}
+
+	hideBanner() {
+		this.classList.remove("visible");
+	}
+
+	hideDetailedSettings() {
+		if (this.dataset.showCookieCategories) this.cookieCategoriesElement.classList.add("visible");
+		if (this.dataset.preferencesAction === "button") this.determinePreferencesButton.classList.add("visible");
+		if (this.dataset.preferencesAction === "link") this.determinePreferencesLink.classList.add("visible");
+
+		this.detailedSettingsElement.classList.remove("visible");
+		this.acceptSelectedButton.classList.remove("visible");
+	}
+
+	showDetailedSettings() {
+		this.cookieCategoriesElement.classList.remove("visible");
+		this.determinePreferencesButton.classList.remove("visible");
+		this.determinePreferencesLink.classList.remove("visible");
+
+		this.detailedSettingsElement.classList.add("visible");
+		this.acceptSelectedButton.classList.add("visible");
+	}
+
+	initializeBanner() {
+		if (this.dataset.showCookieCategories) this.cookieCategoriesElement.classList.add("visible");
+		if (this.dataset.preferencesAction === "button") this.determinePreferencesButton.classList.add("visible");
+		if (this.dataset.preferencesAction === "link") this.determinePreferencesLink.classList.add("visible");
+	}
+
+	initializeConsentCheckboxes() {
+		this.checkboxes = this.querySelectorAll('input[type="checkbox"]');
+
+		this.checkboxes.forEach((checkbox) => {
+			const property = checkbox.dataset.model;
+			checkbox.checked = this.consent[property];
+
+			checkbox.addEventListener("click", (event) => {
+				event.stopPropagation();
 			});
 
-			const data = await response.json();
-		},
-	};
+			checkbox.addEventListener("change", (event) => {
+				event.stopPropagation();
+				this.consent[property] = event.target.checked;
+				this.updateConsentCheckboxes(property);
+			});
+		});
+	}
+
+	updateConsentCheckboxes(consentPropertyToUpdate) {
+		const selector = `input[type="checkbox"][data-model=${consentPropertyToUpdate}]`;
+		const consentCheckboxesToUpdate = this.querySelectorAll(selector);
+
+		consentCheckboxesToUpdate.forEach((checkbox) => {
+			checkbox.checked = this.consent[consentPropertyToUpdate];
+		});
+	}
+
+	registerElements() {
+		this.cookieCategoriesElement = this.querySelector("[data-cookie-categories]");
+		this.detailedSettingsElement = this.querySelector("[data-detailed-settings]");
+
+		this.refuseAllButton = this.querySelector("[data-refuse-all-button]");
+		this.determinePreferencesButton = this.querySelector("[data-determine-preferences-button]");
+		this.determinePreferencesLink = this.querySelector("[data-determine-preferences-link]");
+		this.acceptSelectedButton = this.querySelector("[data-accept-selected-button]");
+		this.acceptAllButton = this.querySelector("[data-accept-all-button]");
+
+		this.manageConsentButtonElements = [...document.querySelectorAll("[data-manage-consent-button]")];
+	}
+
+	registerEventListeners() {
+		this.refuseAllButton.addEventListener("click", () => this.hideBanner());
+		this.determinePreferencesButton.addEventListener("click", () => this.showDetailedSettings());
+		this.determinePreferencesLink.addEventListener("click", () => this.showDetailedSettings());
+		this.acceptSelectedButton.addEventListener("click", () => this.acceptSelected());
+		this.acceptAllButton.addEventListener("click", () => this.acceptAll());
+
+		this.manageConsentButtonElements.forEach((button) => {
+			button.addEventListener("click", () => {
+				this.showDetailedSettings();
+				this.showBanner();
+			});
+		});
+	}
 }
+
+window.customElements.define("cookie-banner", CookieBanner);
--- src/web/assets/src/js/index.js
+++ src/web/assets/src/js/index.js
@@ -1,15 +1,2 @@
-import Alpine from "alpinejs";
-import collapse from "@alpinejs/collapse";
-
 import "./collapse.js";
-import "./bannerv2.js";
-
-import { banner } from "./banner.js";
-
-Alpine.prefix("ccb-");
-
-Alpine.plugin(collapse);
-
-Alpine.data("banner", banner);
-
-Alpine.start();
+import "./banner.js";
